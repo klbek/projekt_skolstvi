bdcf7cf6f872e119df3e3dfd034c857976071bfe
KlaraBek <sadecka.kl@gmail.com>
2024-10-02 00:36:37 +0200

    presun, restrukturalizace souboru, upraveny vzajemne importy

---
+++ data/web_scrap/web_scrap_DS.py
@@ -0,0 +1,56 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+# scrapping regionu a kapacit detskych skupinek  
+# následné vytvoření DF k importu pro analýzu
+
+url_DS = 'https://evidence.mpsv.cz/eEDS/index.php?list'
+session = HTMLSession()
+response = session.get(url_DS)
+soup = BeautifulSoup(response.text, 'html.parser') 
+table = soup.find('div', {'class':'innerContentDiv'})
+
+regions =[]
+if table:
+    # find all the rows of regions names in the "table" body
+    names = table.find_all('b')
+    for reg in names:
+        reg = reg.text.strip()
+        regions.append(reg)
+# print(DS_columns_data)
+
+capacity_after_br = []
+if table:
+    capacity = table.find_all('br')
+    for br in capacity:
+        ns = br.next_sibling
+        if ns and isinstance(ns, str):
+            text = ns.strip()
+            # regex
+            match = re.search(r'celková kapacita:\s*([\d\s]+)\s*míst', text)
+            if match:
+                # extract the number 
+                capacity_number = match.group(1).replace(' ', '')
+                capacity_after_br.append(capacity_number)
+
+i = {'Kapacita': capacity_after_br,
+     'Kraj': regions}
+
+DS_tab = pd.DataFrame(i)
+DS_tab.insert(loc=0, column='ZarizeniTyp', value='Dětská skupinka')
+
+def rename_kraj(kraj):
+    if kraj == "Hlavní město Praha":
+        return kraj
+    elif kraj == "Vysočina":
+        return "Kraj Vysočina"
+    elif kraj.endswith("ký"):
+        return kraj + " kraj"
+    else:
+        return kraj 
+
+DS_tab['Kraj'] = DS_tab['Kraj'].apply(rename_kraj)
+
+# print(DS_tab)
